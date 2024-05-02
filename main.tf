011b618dbb3323f08069ba1107a0b52a7054311b
Pop Vlad-Iulian-Ioan <popvlad24@yahoo.com>
2024-05-02 15:06:50 +0300

    moduleraze the project

--- main.tf
+++ main.tf
@@ -4,27 +4,6 @@ provider "aws" {
     region = "eu-west-3"
 }
 
-variable "vpc_cidr_block" {
-
-}
-variable "subnet_cidr_block" {
-  
-}
-variable "availability_zone" {
-  
-}
-variable "env_prefix" {
-  
-}
-variable "my_ip" {
-  
-}
-variable "instance_type" {
-  
-}
-variable "public_key_location" {
-  
-}
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
@@ -32,95 +11,21 @@ resource "aws_vpc" "myapp-vpc" {
     }
   
 }
-
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.availability_zone
-  tags = {
-      Name:"${var.env_prefix}-subnet-1"
-    }
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_block=var.subnet_cidr_block
+  availability_zone=var.availability_zone
+  env_prefix=var.env_prefix
+  vpc_id=aws_vpc.myapp.id
+  default_route_table_id=aws_vpc.default_route_table_id
 }
-
-
-resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name: "${var.env_prefix}-igw"
-  }
-}
-
-resource "aws_default_route_table" "main-rtb" {
-    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-    route{
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name: "${var.env_prefix}-rtb"
-  }
-  
-}
-
-resource "aws_security_group" "myapp-sg" {
-    name = "myapp-sg"
-    vpc_id = aws_vpc.myapp-vpc.id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.my_ip] 
-    }
-
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] 
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        prefix_list_ids = [] 
-    }
-    tags = {
-    Name: "${var.env_prefix}-sg"
-  }
-}
-
-data "aws_ami" "latest-amazon-linux-image" {
-    most_recent = true
-    owners = [ "amazon" ]
-}
-
-resource "aws_key_pair" "ssh-key" {
-    key_name = "server-key"
-    public_key = "${file(var.public_key_location)}"
-}
-
-resource "aws_instance" "myapp-server" {
-    ami=data.aws_ami.latest-amazon-linux-image.id
-    instance_type = var.instance_type
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    vpc_security_group_ids = [ aws_security_group.myapp-sg ]
-    availability_zone = var.availability_zone
-    associate_public_ip_address = true
-    key_name = aws_key_pair.ssh-key.key_name
-
-    user_data = <<EOF
-                !#bin/bash
-                sudo yum update -y && sudo yum install -y docker
-                sudo systemctl start docker
-                sudo usermod -aG docker ec2-user
-                docker run -p 8080:80 ngnix
-
-                EOF
-
-    tags = {
-    Name: "${var.env_prefix}-server"
-  }
+module "myapp-server" {
+  source="./modules/webserver"
+  vpc_id=aws_vpc.myapp-vpc.id
+  my_ip=var.my_ip
+  env_prefix=var.env_prefix
+  public_key_location=var.public_key_location
+  instance_type=var.instance_type
+  subnet_id=module.myapp-subnet.subnet.id
+  availability_zone=var.availability_zone
 }
---
+++ modules/webserver/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_id" {
+  
+}
+variable "my_ip" {
+  
+}
+variable "env_prefix" {
+  
+}
+variable "public_key_location" {
+  
+}
+variable "instance_type" {
+  
+}
+variable "subnet_id" {
+  
+}
+variable "availability_zone" {
+  
+}
+variable "env_prefix" {
+  
+}
